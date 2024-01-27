ebb444c59828d5cce303025e1ab92e569bf2ca52
YuliaPelia <yuliapelia19@gmail.com>
2024-01-27 19:06:24 +0000

    Deploying to gh-pages from @ apenduks1/Foodsta@55ddf4319224db63bac7a3df688e5f73d1e7898f 🚀

---
+++ index.60e303fe.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".promo__photo").slick({dots:!1,infinite:!0,speed:500,slidesToShow:1,slidesToScroll:1})}));
+//# sourceMappingURL=index.60e303fe.js.map
---
+++ index.6fb91073.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelectorAll("[data-open-order]"),closeModalBtn:document.querySelector("[data-close-order]"),backdrop2:document.querySelector("[data-backdrop-order]")};function o(){e.backdrop2.classList.toggle("is-hidden-order")}e.closeModalBtn.addEventListener("click",o),e.openModalBtn.forEach((function(e){return e.addEventListener("click",o)}))}();
+//# sourceMappingURL=index.6fb91073.js.map
